67dc06f32fd6cd0df6bf31a8a1db8dc326ed4eeb
cbit-interactive <elisover7@gmail.com>
2024-02-15 20:00:01 -0800

    Testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// use ferris_says::say;
+// use std::io::{stdout, BufWriter};
+
 fn main() {
     println!("Hello, world!");
 }
+
+// fn ferris(string: message){
+//     let _ = string;
+//     let stdout = stdout();
+//     let doug = String::from(message);
+//     let width = doug.chars().count();
+//     let mut writer = BufWriter::new(stdout.lock());
+
+//     say(&doug, width, &mut writer).unwrap();
+// }
